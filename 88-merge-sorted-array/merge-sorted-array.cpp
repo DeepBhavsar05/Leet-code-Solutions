8eaeb49905d4229f69e3d3554270d121026cd57d
DeepBhavsar05 <165227757+DeepBhavsar05@users.noreply.github.com>
2025-04-25 02:27:23 -0400

    Time: 0 ms (100.00%) | Memory: 12.4 MB (8.93%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -19,48 +19,48 @@ public:
     //     sort(nums1.begin(),nums1.end());
     // }
     //chatgpt approach
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
+        // int i = m - 1;
+        // int j = n - 1;
+        // int k = m + n - 1;
+        // while (i >= 0 && j >= 0) {
+        //     if (nums1[i] > nums2[j]) {
+        //         nums1[k--] = nums1[i--];
+        //     } else {
+        //         nums1[k--] = nums2[j--];
+        //     }
+        // }
+
+        // // Copy remaining elements of nums2 (if any)
+        // while (j >= 0) {
+        //     nums1[k--] = nums2[j--];
+        // }
+    //Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+        int i=0;
+        int j=0;
+        int index=0;
+        vector<int> nums3(m+n);
+        while(i<m && j<n){
+            if(nums1[i]<=nums2[j]){
+                nums3[index]=nums1[i];
+                i++;
+                index++;
+            }
+            else{
+                nums3[index]=nums2[j];
+                j++;
+                index++;
             }
         }
-
-        // Copy remaining elements of nums2 (if any)
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
+        //if there is still some elements in nums1
+        while(i<m){
+            nums3[index++]=nums1[i++];
         }
-    //Follow up: Can you come up with an algorithm that runs in O(m + n) time?
-        // int i=0;
-        // int j=0;
-        // int index=0;
-        // vector<int> nums3(m+n);
-        // while(i<m && j<n){
-        //     if(nums1[i]<=nums2[j]){
-        //         nums3[index]=nums1[i];
-        //         i++;
-        //         index++;
-        //     }
-        //     else{
-        //         nums3[index]=nums2[j];
-        //         j++;
-        //         index++;
-        //     }
-        // }
-        // //if there is still some elements in nums1
-        // while(i<m){
-        //     nums3[index++]=nums1[i++];
-        // }
-        // //if there is still some elements in nums2
-        // while(j<n){
-        //     nums3[index++]=nums2[j++];
-        // }
-        // for(int i=0;i<nums1.size();i++){
-        //     nums1[i]= nums3[i];
-        // }
+        //if there is still some elements in nums2
+        while(j<n){
+            nums3[index++]=nums2[j++];
+        }
+        for(int i=0;i<nums1.size();i++){
+            nums1[i]= nums3[i];
+        }
     }
 };
