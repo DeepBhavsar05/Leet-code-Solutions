ea64248b789e8ac803df16830c0afab60bdf08a6
DeepBhavsar05 <165227757+DeepBhavsar05@users.noreply.github.com>
2025-05-23 19:02:33 -0400

    Time: 7 ms (40.21%) | Memory: 17.8 MB (69.94%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -5,20 +5,39 @@ class Solution:
         grid_set=set()
         for i in range(9):
             grid_set.clear()
-            for j in range(3):
-                for k in range(3):
-                    num=board[j+(3*(i//3))][k+(3*(i%3))]
-                    if num=='.':
-                        continue
-                    if num in row_sets[j+(3*(i//3))]:
-                        return False
-                    if num in col_sets[k+(3*(i%3))]:
-                        return False
-                    if num in grid_set:
-                        return False
-                    
-                    row_sets[j+(3*(i//3))].add(num)
-                    col_sets[k+(3*(i%3))].add(num)
-                    grid_set.add(num)
+            for j in range(9):
+                num=board[i-i%3+j//3][((i%3)*3)+j%3]
+                if num=='.':
+                    continue
+                if num in row_sets[i-i%3+j//3]:
+                    return False
+                if num in col_sets[((i%3)*3)+j%3]:
+                    return False
+                if num in grid_set:
+                    return False
+
+                row_sets[i-i%3+j//3].add(num)
+                col_sets[((i%3)*3)+j%3].add(num)
+                grid_set.add(num)
         return True
+
+
+        # for i in range(9):
+        #     grid_set.clear()
+        #     for j in range(3):
+        #         for k in range(3):
+        #             num=board[j+(3*(i//3))][k+(3*(i%3))]
+        #             if num=='.':
+        #                 continue
+        #             if num in row_sets[j+(3*(i//3))]:
+        #                 return False
+        #             if num in col_sets[k+(3*(i%3))]:
+        #                 return False
+        #             if num in grid_set:
+        #                 return False
+                    
+        #             row_sets[j+(3*(i//3))].add(num)
+        #             col_sets[k+(3*(i%3))].add(num)
+        #             grid_set.add(num)
+        # return True
         
