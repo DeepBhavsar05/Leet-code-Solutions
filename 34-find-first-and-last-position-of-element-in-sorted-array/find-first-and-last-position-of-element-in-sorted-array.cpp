33eed5229d7567051af4149fd930697fe37500f6
DeepBhavsar05 <165227757+DeepBhavsar05@users.noreply.github.com>
2025-05-29 22:45:26 -0400

    Time: 0 ms (100.00%) | Memory: 17.5 MB (85.96%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,17 +1,74 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        //first I will write my code for brute force O(N)
+    int lb(vector<int>& nums,int target,int n){
+        //implementation of lower bound
+        int low = 0;
+        int high = n-1;
         int first = -1;
-        int last = -1;
-        int n = nums.size();
-        for(int i =0;i<n;i++){
-            if(nums[i] == target){
-                if(first == -1) first = i;
-                last = i;
-                // return {first,last};
+
+        while(low<=high){
+            int mid = (low+high)/2;
+
+            if(nums[mid] == target){
+                first = mid;
+                high = mid-1;//move left
+            }
+            else if(nums[mid]<target){
+                //mid value is smaller so i have to go on right to find targeted value
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
             }
         }
-        return{first,last};
+        return first;
+    }
+    int ub(vector<int>& nums,int target,int n){
+        //implementation of upper bound
+        int low = 0;
+        int high = n-1;
+        int last = -1;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+
+            if(nums[mid] == target){
+                last = mid;
+                low = mid+1;//move right
+            }
+            else if(nums[mid]<target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return last;
+    }
+    // pair<int,int> firstLast(vector<int>& nums,int target,int n){
+    //     int first = lb(nums,target,n);
+    //     if(first == -1) return {-1,-1};
+    //     int last = ub(nums,target,n);
+    //     return {first,last};
+    // }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        //first I will write my code for brute force O(N)
+        // int first = -1;
+        // int last = -1;
+        int n = nums.size();
+        // for(int i =0;i<n;i++){
+        //     if(nums[i] == target){
+        //         if(first == -1) first = i;
+        //         last = i;
+        //     }
+        // }
+        // return{first,last};
+        //here i will use lower bound and upper bound concept for logn solution
+        //returning values of first and last from firstLast() fun
+        int first = lb(nums,target,n);
+        if(first == -1) return {-1,-1};
+        int last = ub(nums,target,n);
+        return {first,last};
     }
 };
