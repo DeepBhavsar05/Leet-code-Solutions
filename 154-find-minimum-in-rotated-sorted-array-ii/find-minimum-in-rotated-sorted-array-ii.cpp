de7696fb9033706943f5c6f5532ed194f45c0097
DeepBhavsar05 <165227757+DeepBhavsar05@users.noreply.github.com>
2025-06-02 14:40:42 -0400

    Time: 0 ms (100.00%) | Memory: 16.3 MB (28.88%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -9,11 +9,15 @@ public:
         while(low<=high){
             int mid = (low+high)/2;
             if(nums[low]< nums[high]){
+                // why = is not in consideration here
+                //because it gives wrong answer for this: [3,4,1,2,3]
                 //just for little optimization
                 mini = min(mini,nums[low]);
                 break;//do not need to check further for next halves
             }
             if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                //cases like [1] low,mid,high could be same so thats why 
+                //we will check low --->mid--->high
                 mini = min(mini, nums[low]);
                 low = low+1;
                 high = high -1;
@@ -21,6 +25,12 @@ public:
             }
             else if(nums[low]<=nums[mid]){
                 //left-half is sorted
+                //why we use else if here?
+                //because if i use 'if' here then for the case such as [1]
+                //might go here for consideration
+                //even it takes mini = 1 
+                //it will increase the value of low ---> low+1 
+                //which would be wrong for [1]
                 mini = min(mini,nums[low]);
                 low = mid+1;
             }
