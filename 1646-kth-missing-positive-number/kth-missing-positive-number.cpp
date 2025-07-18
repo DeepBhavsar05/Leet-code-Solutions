a37d8fc6a9b6aa98fa3fb60db15198d4340918b5
DeepBhavsar05 <165227757+DeepBhavsar05@users.noreply.github.com>
2025-07-17 22:43:00 -0400

    Time: 0 ms (100.00%) | Memory: 13.3 MB (59.05%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -10,5 +10,23 @@ public:
             else break;
         }
         return k;
+        //lets do this in logN
+        // using bs on answers
+        int low =0;
+        int high = arr.size()-1;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            int current_missing = arr[mid] -(mid+1);//give us actual missing values till current element
+
+            if(current_missing < k){
+                low = mid+1;
+            }
+            else{
+                high = mid - 1;
+            }
+
+            return high+1+k;//low+k
+        }
     }
 };
